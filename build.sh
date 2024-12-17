28c3c0d9f1b895d8af35b26ff2da4fa99eba78e9
gpBlockchain <744158715@qq.com>
2024-12-17 16:42:42 +0800

    add build

--- build.sh
+++ build.sh
@@ -1,2 +1,13 @@
 # git clone light
-cd light-client/
+cd ckb-light-client
+cd light-client-db-worker
+npm i
+npm run build
+cd ../light-client-wasm
+npm i
+npm run build
+cd ../light-client-js
+npm i
+npm run build
+cd ../../
+python server.py
