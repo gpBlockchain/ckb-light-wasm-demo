80741d7a05aa0642509c43dbd247e689b0a381b3
gpBlockchain <744158715@qq.com>
2024-12-17 21:01:16 +0800

    fix handle

--- build.sh
+++ build.sh
@@ -14,4 +14,6 @@ python3 server.py > server.log 2>&1 &
 cd ckb-light-client-rpc
 npm i
 npm run build
-npm run service
+npm run service > rpc.log 2>&1 &
+sleep 5
+echo "CKB Light Client RPC server is running html: http://localhost:8000  rpc: http://localhost:9000"
