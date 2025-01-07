f5b0454a1dd417984e3337a11d1cfcab6556a9ae
gpBlockchain <744158715@qq.com>
2025-01-07 15:35:57 +0800

    update project

--- build.sh
+++ build.sh
@@ -1,15 +1,8 @@
 # git clone light
 cd ckb-light-client
-cd light-client-db-worker
 npm i
-npm run build
-cd ../light-client-wasm
-npm i
-npm run build
-cd ../light-client-js
-npm i
-npm run build
-cd ../../
+npm run build -ws
+cd ../
 python3 server.py > server.log 2>&1 &
 cd ckb-light-client-rpc
 npm i
