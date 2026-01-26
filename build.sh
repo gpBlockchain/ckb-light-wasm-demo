9b7ea45797277e8905bc8b519f9e8ff0987f5803
gpBlockchain <32102187+gpBlockchain@users.noreply.github.com>
2026-01-26 14:06:40 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,31 +1,18 @@
 # git clone light
-CKB_LIGHT_CLIENT_URL=${CKB_LIGHT_CLIENT_URL:-https://github.com/nervosnetwork/ckb-light-client.git}
-CKB_LIGHT_CLIENT_BRANCH=${CKB_LIGHT_CLIENT_BRANCH:-develop}
-if [ ! -d "ckb-light-client" ]; then
-    git clone ${CKB_LIGHT_CLIENT_URL}
+# CKB_LIGHT_CLIENT_URL=${CKB_LIGHT_CLIENT_URL:-https://github.com/nervosnetwork/ckb-light-client.git}
+# CKB_LIGHT_CLIENT_BRANCH=${CKB_LIGHT_CLIENT_BRANCH:-develop}
+
+# is set  CKB_LIGHT_CLIENT_URL env, remove dir ,clone again
+if [ -n "${CKB_LIGHT_CLIENT_URL}" ] && [ -d "ckb-light-client" ]; then
+        rm -rf ckb-light-client
+        git clone ${CKB_LIGHT_CLIENT_URL}
+        cd ckb-light-client
+        git checkout ${CKB_LIGHT_CLIENT_BRANCH}
+        cd ../
 fi
+
 cd ckb-light-client
-# 强制更新 remote URL
-git remote set-url origin ${CKB_LIGHT_CLIENT_URL}
-# 强制切换到指定分支
-git fetch origin --prune
-# 检查远程分支是否存在
-REMOTE_BRANCH_EXISTS=$(git ls-remote --heads origin ${CKB_LIGHT_CLIENT_BRANCH} | wc -l)
-if [ "$REMOTE_BRANCH_EXISTS" -gt 0 ]; then
-    # 远程分支存在，先 fetch 该分支，然后切换
-    git fetch origin ${CKB_LIGHT_CLIENT_BRANCH}
-    git checkout -B ${CKB_LIGHT_CLIENT_BRANCH} origin/${CKB_LIGHT_CLIENT_BRANCH}
-elif git show-ref --verify --quiet refs/heads/${CKB_LIGHT_CLIENT_BRANCH}; then
-    # 远程分支不存在，但本地分支存在
-    git checkout ${CKB_LIGHT_CLIENT_BRANCH}
-else
-    # 分支不存在
-    echo "Error: Branch ${CKB_LIGHT_CLIENT_BRANCH} does not exist in remote or local"
-    echo "Available remote branches:"
-    git ls-remote --heads origin | sed 's|.*refs/heads/||' || true
-    exit 1
-fi
-git log -1
+
 cargo install wasm-pack
 npm install
 npm run build -ws
