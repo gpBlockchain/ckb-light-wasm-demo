2ca0a766342d550e99720750036ad8e9aea57168
gpBlockchain <32102187+gpBlockchain@users.noreply.github.com>
2026-01-26 10:44:50 +0800

    Update build script for CKB Light Client setup

--- build.sh
+++ build.sh
@@ -1,5 +1,15 @@
 # git clone light
+CKB_LIGHT_CLIENT_URL=${CKB_LIGHT_CLIENT_URL:-https://github.com/nervosnetwork/ckb-light-client.git}
+CKB_LIGHT_CLIENT_BRANCH=${CKB_LIGHT_CLIENT_BRANCH:-develop}
+if [ ! -d "ckb-light-client" ]; then
+    git clone ${CKB_LIGHT_CLIENT_URL}
+fi
 cd ckb-light-client
+# 强制更新 remote URL
+git remote set-url origin ${CKB_LIGHT_CLIENT_URL}
+# 强制切换到指定分支
+git fetch origin
+git checkout -B ${CKB_LIGHT_CLIENT_BRANCH} origin/${CKB_LIGHT_CLIENT_BRANCH} 2>/dev/null || git checkout ${CKB_LIGHT_CLIENT_BRANCH}
 cargo install wasm-pack
 npm install
 npm run build -ws
